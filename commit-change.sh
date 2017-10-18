db5556fc9a59f4c32c3cae4599f41e73747fa5b9
Chris Houseknecht <chousekn@redhat.com>
2017-10-17 23:57:10 -0400

    =Hello AWX

--- commit-change.sh
+++ commit-change.sh
@@ -9,5 +9,5 @@ git add . --all
 git commit -S -m="Hello AWX" 
 
 tower-cli workflow_job launch \
-  --workflow_job_template="Dev Deployment" \
+  --workflow-job-template="Dev Deployment" \
   --extra-vars="instance_tag=house001 instace_type=t2.medium"
