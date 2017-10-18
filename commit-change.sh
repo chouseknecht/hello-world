f503027ebf13982ace9a5ae4d46e36303c9e24d8
Chris Houseknecht <chousekn@redhat.com>
2017-10-17 23:44:59 -0400

    Update scripts

--- commit-change.sh
+++ commit-change.sh
@@ -8,8 +8,4 @@ git add . --all
 
 git commit -S
 
-tower-cli config host "http://${TOWER_HOST}"
-tower-cli config username ${TOWER_USER}
-tower-cli config password ${TOWER_PASSWORD}
-tower-cli config verify_ssl false
-tower-cli job launch --job-template="${TOWER_TEMPLATE}" --extra-vars='instance_tag: house001'
+tower-cli job launch --job-template="Dev Deployment" --extra-vars='instance_tag: house001'
