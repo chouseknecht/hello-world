a3e6a025b44dc0e18b5b7ea4a5f49bac43383775
Chris Houseknecht <chousekn@redhat.com>
2017-10-17 23:58:06 -0400

    Reset

--- commit-change.sh
+++ commit-change.sh
@@ -10,4 +10,4 @@ git commit -S -m="Hello AWX"
 
 tower-cli workflow_job launch \
   --workflow-job-template="Dev Deployment" \
-  --extra-vars="instance_tag=house001 instace_type=t2.medium"
+  --extra-vars="instance_tag=house001 instance_type=t2.medium"
